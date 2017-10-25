b369857b65ff93ad004df2d9f33de5bfdb689e9c
Dino Mandic <dinomandic@hotmail.com>
2017-10-24 21:51:07 -0500

    Completed all required steps

---
+++ src/router.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Cart from './components/Cart/Cart';
+import Details from './components/Details/Details';
+import Landing from './components/Landing/Landing';
+import Shop from './components/Shop/Shop';
+
+import ThankYou from './components/ThankYou/ThankYou';
+
+
+export default (
+  <Switch>
+    <Route exact path="/" component={ Landing }/>
+    <Route path="/cart" component={ Cart }/>
+    <Route path="/details/:name" component={ Details }/>
+    <Route path="/shop" component={ Shop }/>
+    <Route path="/thank-you" component={ ThankYou }/>
+  </Switch>
+)
